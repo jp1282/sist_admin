dccef8cbc67e2e71cbcae085cd19274adaae767f
Juan Piña <67019297+jp1282@users.noreply.github.com>
2023-05-03 14:35:16 -0400

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,26 @@
+</div></div>
+<br><br>
+<!-- footer -->
+<div class="footer" style="background-color:grey;">
+    <br>
+    <table width="100%">
+        <tr>
+            <th>Sucursales</th>
+            <th>Telefonos</th>
+            <th>Contacto</th>
+        </tr>
+        <tr>
+            <td>Casa Matriz: Pajaritos 1800</td>
+            <td>+56 9 552 66 12</td>
+            <td>contacto@artoffice.cl</td>
+        </tr>
+        <tr>
+            <td>Mall Plaza Oeste local 888</td>
+            <td>+56 9 255 77 44</td>
+            <td>ventas@artoffice.cl</td>
+        </tr>
+    <table>
+</div>
+<br>
+</body>
+</html>
